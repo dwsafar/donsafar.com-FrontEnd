966918a5c61fe036eaaf166f1bf85fb9a3a975a5
Don Safar <donjr.safar@gmail.com>
2022-08-10 17:09:35 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+		var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+
+		request.open('GET', 'https://w4fhxg44d8.execute-api.us-east-1.amazonaws.com/Production/', true)
+		request.onload = function () {
+  
+var data = JSON.parse(this.response)
+
+if (request.status >= 200 && request.status < 400) {
+      console.log(data.count)
+      document.getElementById('count').innerHTML = (data.count);
+  }
+else {
+  console.log('error')
+
+}
+}
+
+
+// Send request
+request.send()
+
